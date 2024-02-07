0b0abc6de86435bebb88274ee7f9094c7e333aa5
op-6160 <sagvd01@gmail.com>
2024-02-08 03:42:32 +0900

    debug off for service

--- Big/settings.py
+++ Big/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-mh=_xfbdr^)6@lr9_!209!mrm4mrs5-3!gtrmnjs%*z-eqd_u$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
